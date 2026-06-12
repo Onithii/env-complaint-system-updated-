98993d02b2011b92b316a2c33b06d1acd79ae519
Onithii <wkonithi11@gmail.com>
2026-06-12 11:02:14 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/example/haritha/HarithaApplication.java
@@ -0,0 +1,13 @@
+package com.example.haritha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarithaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarithaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haritha/HarithaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haritha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarithaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
